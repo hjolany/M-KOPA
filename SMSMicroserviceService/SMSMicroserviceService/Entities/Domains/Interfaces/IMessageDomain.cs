1e3cc97c922080d2eecc42a2ef8d1ba65962bdf1
Hamid Jolany <hjolany@eagletechsolutions.co.uk>
2023-08-31 17:44:42 +0330

    Create IMessageDomain.cs

---
+++ SMSMicroserviceService/SMSMicroserviceService/Entities/Domains/Interfaces/IMessageDomain.cs
@@ -0,0 +1,9 @@
+﻿namespace SMSMicroService.Entities.Domains.Interfaces
+{
+    public interface IMessageDomain
+    {
+        public string PhoneNumber { get; set; }
+        public string Content { get; set; }
+        public string ToString();
+    }
+}
