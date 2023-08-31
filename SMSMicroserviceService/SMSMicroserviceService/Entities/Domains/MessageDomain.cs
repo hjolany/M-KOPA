306f7d7a80a54c253995871a2bd9350b932e9dae
Hamid Jolany <hjolany@eagletechsolutions.co.uk>
2023-08-31 17:44:45 +0330

    Update MessageDomain.cs

--- SMSMicroserviceService/SMSMicroserviceService/Entities/Domains/MessageDomain.cs
+++ SMSMicroserviceService/SMSMicroserviceService/Entities/Domains/MessageDomain.cs
@@ -1,12 +1,12 @@
 ﻿using MediatR;
+using SMSMicroService.Entities.Domains.Interfaces;
 
 namespace SMSMicroService.Entities.Domains
 {
-    public class MessageDomain : INotification
+    public class MessageDomain : IMessageDomain
     {
         public string PhoneNumber { get; set; }
         public string Content { get; set; }
-        public DateTime SendDate { get; set; } = DateTime.Now;
         public override string ToString()
         {
             return $"{PhoneNumber}=>\t{Content}";
